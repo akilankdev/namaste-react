9d013c6b4178415af4e414d2763d9911efe6351d
Akilan K <akilankdev@gmail.com>
2026-04-25 11:32:06 +0530

    Components Basics

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-console.log(heading);
+const Title = () => <h1 className="title">React Components</h1>;
+const divElem = <div>Tomorrow is a holiday</div>;
+const BodyComponent = () => (
+  <div className="container">
+    <Title />
+    Component body
+  </div>
+);
 
-const jsxHeading = <h1 className="heading" tabIndex="1">Heading from JSX</h1>;
-console.log(jsxHeading);
+const a = 33;
+const b = 36;
+
+const Span = function () {
+  return (
+    <span>
+      {Title()}
+      <Title />
+      <Title></Title>
+      Sum of a and b = {a + b}
+      {console.log("hii bro")}
+      {divElem}
+    </span>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<Span />);
