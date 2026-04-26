4ba15127fc597fdc7a2fa465db1b1caf1e700141
Akilan K <akilankdev@gmail.com>
2026-04-26 19:11:55 +0530

    Added body with just two cards

--- App.js
+++ App.js
@@ -1,31 +1,73 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => <h1 className="title">React Components</h1>;
-const divElem = <div>Tomorrow is a holiday</div>;
-const BodyComponent = () => (
-  <div className="container">
-    <Title />
-    Component body
-  </div>
-);
+/* 
+Header
+-logo
+-nav-items
+Body
+-search
+-Restaurant container
+-Restaurant card
+Footer
+-copyright
+-contact us
+-address
+*/
 
-const a = 33;
-const b = 36;
-
-const Span = function () {
+const Header = () => {
   return (
-    <span>
-      {Title()}
-      <Title />
-      <Title></Title>
-      Sum of a and b = {a + b}
-      {console.log("hii bro")}
-      {divElem}
-    </span>
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://static.vecteezy.com/system/resources/previews/011/401/427/non_2x/food-signal-online-food-ordering-logo-design-order-food-on-internet-restaurant-cafe-meals-delivery-online-free-vector.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  console.log(props);
+  return (
+    <div className="res-card" style={{"backgroundColor": "#f0f0f0"}}>
+      <img className="res-logo" src="https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=appetizer-bowl-delicious-1640772.jpg&fm=jpg" />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.3 stars</h4>
+      <h4>40 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (<div className="body">
+    <div className="search">Search</div>
+    <div className="res-container">
+      <RestaurantCard resName="KFC" cuisine="Burgers"/>
+      <RestaurantCard />
+    </div>
+  </div>);
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Span />);
+root.render(<AppLayout />);
