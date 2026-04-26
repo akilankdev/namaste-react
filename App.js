4cc73a4fd26b703251f502ee43cec84cd0dfb295
Akilan K <akilankdev@gmail.com>
2026-04-26 20:04:03 +0530

    added more cards and accessed using index

--- App.js
+++ App.js
@@ -49,6 +49,83 @@ const restaurantList = [
         "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=appetizer-bowl-delicious-1640772.jpg&fm=jpg",
     },
   },
+  {
+    type: "restaurant",
+    data: {
+      name: "Domino's Pizza",
+      cuisines: ["Pizza", "Italian", "Fast Food"],
+      avgRating: 4.2,
+      costForTwo: 50000,
+      deliveryTime: 30,
+      imageLink: "https://clipart-library.com/2024/food-images/food-images-3.jpg",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      name: "A2B - Adyar Ananda Bhavan",
+      cuisines: ["South Indian", "Sweets", "Snacks"],
+      avgRating: 4.4,
+      costForTwo: 30000,
+      deliveryTime: 25,
+      imageLink: "https://img.freepik.com/premium-photo/food-photos_639785-169936.jpg",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      name: "Burger King",
+      cuisines: ["Burgers", "American", "Fast Food"],
+      avgRating: 4.1,
+      costForTwo: 45000,
+      deliveryTime: 28,
+      imageLink: "https://static.vecteezy.com/system/resources/previews/020/824/046/large_2x/assortment-of-healthy-protein-source-and-body-building-food-free-photo.jpg",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      name: "Saravana Bhavan",
+      cuisines: ["South Indian", "Vegetarian"],
+      avgRating: 4.5,
+      costForTwo: 35000,
+      deliveryTime: 32,
+      imageLink: "https://thumbs.dreamstime.com/b/fresh-breakfast-eggs-food-photos-285707612.jpg",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      name: "Subway",
+      cuisines: ["Healthy Food", "Salads", "Sandwiches"],
+      avgRating: 4.0,
+      costForTwo: 40000,
+      deliveryTime: 27,
+      imageLink: "https://img.freepik.com/premium-photo/food-photos_639785-169426.jpg",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      name: "Pizza Hut",
+      cuisines: ["Pizza", "Italian", "Fast Food"],
+      avgRating: 4.2,
+      costForTwo: 55000,
+      deliveryTime: 35,
+      imageLink: "https://www.pngmart.com/files/23/Food-PNG-Isolated-Pic.png",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      name: "Barbeque Nation",
+      cuisines: ["BBQ", "North Indian", "Grill"],
+      avgRating: 4.3,
+      costForTwo: 80000,
+      deliveryTime: 40,
+      imageLink: "https://sukhis.com/app/uploads/2022/11/delicious-indian-veg-food.jpg",
+    },
+  },
 ];
 
 const RestaurantCard = (props) => {
@@ -72,6 +149,13 @@ const Body = () => {
       <div className="search">Search</div>
       <div className="res-container">
         <RestaurantCard resData={restaurantList[0]} />
+        <RestaurantCard resData={restaurantList[1]} />
+        <RestaurantCard resData={restaurantList[2]} />
+        <RestaurantCard resData={restaurantList[3]} />
+        <RestaurantCard resData={restaurantList[4]} />
+        <RestaurantCard resData={restaurantList[5]} />
+        <RestaurantCard resData={restaurantList[6]} />
+        <RestaurantCard resData={restaurantList[7]} />        
       </div>
     </div>
   );
