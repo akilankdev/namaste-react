d09080695ecc875b34cb5a2fc0b45b903debcabe
Akilan K <akilankdev@gmail.com>
2026-05-14 14:50:32 +0530

    added a error page that is displayed when the path is invalid

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact"
+import Error from "./components/Error";
 import { createBrowserRouter,RouterProvider } from "react-router-dom";
 
 const App = () => {
@@ -18,7 +19,8 @@ const App = () => {
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+    errorElement: <Error />
   },
   {
     path: "/about",
