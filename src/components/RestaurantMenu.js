91a92b59c55af7523cd893682d405f24035f159f
Akilan K <akilankdev@gmail.com>
2026-06-20 12:20:30 +0530

    Added dynamic route for showing Restaurant Menu page

---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,14 @@
+const RestaurantMenu = () => {
+  return (
+    <div className="menu">
+      <h1>Restaurant Name</h1>
+      <ul>
+        <li>Biryani</li>
+        <li>Burgers</li>
+        <li>Cold Drinks</li>
+      </ul>
+    </div>
+  );
+}
+
+export default RestaurantMenu;
