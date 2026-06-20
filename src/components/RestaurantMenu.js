46efde33158973bc87ec5b181306ed23099255d5
Akilan K <akilankdev@gmail.com>
2026-06-20 12:58:04 +0530

    fetched Restaurant menu details of Pizza paradise

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,4 +1,18 @@
+import {useEffect} from "react";
+
 const RestaurantMenu = () => {
+
+  useEffect(()=> {
+    fetchData();
+  },[]);
+
+  const fetchData = async () => {
+    const data = await fetch("https://namastedev.com/api/v1/listRestaurantMenu/123456");
+    const json = await data.json();
+
+    console.log(json);
+  }
+
   return (
     <div className="menu">
       <h1>Restaurant Name</h1>
