978bc8b520e00447b50d02d8e0ff5b7617a6ecfb
Akilan K <akilankdev@gmail.com>
2026-06-21 20:38:31 +0530

    Created Accordion style UI for showing menu items for each categories

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,18 +1,30 @@
-const RestaurantCategory = (props) => {
-  //the 'data' field below is from the RestaurantMenu component where we pass the props value.You wont find this 'data' field in our response.
-  const menuItems = props.data.itemCards;
+import { useState } from "react";
+
+
+//{data} - destructuring the props name and value
+const RestaurantCategory = ({ data }) => {
+  //For toggling ACCORDION style UI,initially false means menuitems wont show,when clicked showItems state becomes true and component rerenders with menuitems.
+  const [showItems, setShowItems] = useState(false);
+
+  const menuItems = data.itemCards;
 
   return (
     <>
-      <li>{props.data.title}</li>
+      {/* shows the Category name */}
+      <li onClick={() => setShowItems(!showItems)}>
+        {data.title} ({data.itemCards.length})
+      </li>
 
-      <ul>
-        {
-          menuItems.map((MenuItem) =>{
-            return <li key={MenuItem.card.info.id}>{MenuItem.card.info.name}</li>
-          })
-        }
-      </ul>
+      {/* Shows Menu items if the category is clicked,otherwise menu items won't show */}
+      {showItems && (
+        <ul>
+          {menuItems.map((MenuItem) => {
+            return (
+              <li key={MenuItem.card.info.id}>{MenuItem.card.info.name}</li>
+            );
+          })}
+        </ul>
+      )}
     </>
   );
 };
