716c51cdd1102552fb298e30909751dc8e742517
Akilan K <akilankdev@gmail.com>
2026-06-23 12:49:26 +0530

    created class based User component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import User from "./User";
+import UserClass from "./UserClass";
 const About = () => {
   return (
     <div>
       <h1>About Us</h1>
       <User name="Akilan" location="Tamil Nadu" />
+      <UserClass />
     </div>
   );
 }
---
+++ src/components/UserClass.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: kkk</h2>
+        <h3>Location: skfj</h3>
+        <h3>Contact: some email </h3>
+      </div>
+    );
+  }
+}
+export default UserClass;
