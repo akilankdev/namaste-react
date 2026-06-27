03e2d49b910a334f76d3f0cb4bf03668175212b9
Akilan K <akilankdev@gmail.com>
2026-06-27 09:18:15 +0530

    React lifecycle method flow for Mounting

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,43 @@
-import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <UserClass name="Akilan K" location="Tamil Nadu"/>
-    </div>
-  );
+import React from "react";
+
+class About extends React.Component {
+  constructor(props){
+    super(props);
+    console.log("Parent cons");
+  }
+
+  componentDidMount() {
+    console.log("Parent component did mount");
+  }
+
+  render() {
+    console.log("Parent render");
+    return (
+      <div>
+        <h1>About Us</h1>
+        <UserClass name="Child 1" location="Tamil Nadu" />
+        <UserClass name="Child 2" location="Tamil Nadu" />
+      </div>
+    );
+  }
 }
 
 export default About;
+
+/*
+RENDER PHASE BEGINS...
+-parent constructor
+-parent render
+  -child1 cons
+  -child1 render
+
+  -child2 cons
+  -child2 render
+
+COMMIT PHASE BEGINS...
+-child1 componentDidMount
+-child2 componentDidMount
+-Parent componentDidMount
+  
+*/
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,29 +2,37 @@ import React from "react";
 
 class UserClass extends React.Component {
   constructor(props) {
-
     super(props);
 
-    //this.state used to create state variables.We use object to put state variables inside it.
     this.state = {
       count: 0,
-      count2:1
+      count2: 1,
     };
+    console.log(this.props.name + " cons");
+  }
+
+  //Called after render() method is executed.
+  componentDidMount() {
+    console.log(this.props.name + " component did mount"); 
   }
 
   render() {
-    const {name,location} = this.props;
-    const {count,count2} = this.state;
+    const { name, location } = this.props;
+    const { count, count2 } = this.state;
+    console.log(this.props.name + "render");
     return (
       <div className="user-card">
         <h2>Count: {count}</h2>
-        <button onClick={()=> {
-          //setState() method is used to update the variables value.We can also modify multiple variable's value at the same object.
-          this.setState({
-            count: count + 1,//'count' in the right side denotes the destructured varaible.Otherwise we put it as this.state.count + 1
-            count2: count2 + 1 
-          });
-        }}>Increase count</button>
+        <button
+          onClick={() => {
+            this.setState({
+              count: count + 1,
+              count2: count2 + 1,
+            });
+          }}
+        >
+          Increase count
+        </button>
         <h2>Count2: {count2}</h2>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
