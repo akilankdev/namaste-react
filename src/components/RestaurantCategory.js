9c1173252f6c122d5ee1de9dee7f4c7560696c1d
Akilan K <akilankdev@gmail.com>
2026-08-10 21:50:05 +0530

    Made the accordion header for each categories

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -4,7 +4,13 @@ const RestaurantCategory = ( {data} ) => {
   console.log(data);
   return (
    <div>
-    ResCategory
+    {/* w-6/12 is used to allocate half the width because tailwind divides page into 12 sections. */}
+
+      <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg flex justify-between p-4">
+        <span className="font-bold text-sm">{data.title} ({data.itemCards.length})</span>
+        {/* press win + ; for emojis */}
+        <span>🔽</span>
+      </div>
    </div>
   );
 };
