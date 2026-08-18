d4af325485d79ea292915f03e10fab3b40446dfd
Akilan K <akilankdev@gmail.com>
2026-08-18 18:53:35 +0530

    Created a slice for cart

---
+++ src/utils/cartSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state,action) => {
+      //reducer fn can take two parameters:"state" and "action".
+      state.items.push(action.payload);
+    },
+    removeItem: (state) => {
+      //removes the last item.
+      state.items.pop();
+    },
+    clearCart: (state) => {
+      //resets the cart to 0 again.We cant do 'state.items = []',theres a reason behind it.
+      state.items.length = 0;
+    }
+  }
+});
+//We export reducer and the actions.Below is the syntax to export it.
+export const {addItem,removeItem,clearCart} = cartSlice.actions;
+export default cartSlice.reducer;
+
